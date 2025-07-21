73ff6390dacb25d6b59f2c7500079e428002fb91
runnz121 <runnz121@gmail.com>
2025-07-21 21:18:53 +0900

    [feat] 프로젝트 초기 의존성 추가 및 설정 - springboot3, java 21, webflux, r2dbc

---
+++ src/main/java/com/kakao/billing_homework/BillingHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.billing_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/billing_homework/BillingHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.billing_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
