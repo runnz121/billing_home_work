b6f44617c85e222ee3ade4549c94e77162454a51
runnz121 <runnz121@gmail.com>
2025-07-25 00:47:13 +0900

    [refactor] 엄격모드 적용하도록 수정 - 존재하지 않는 날짜 들어올시 에러 발생 및 2999 날짜 반환하도록 설정

--- batch-app/src/main/java/com/kakao/batchapp/dataIngest/util/DateUtils.java
+++ batch-app/src/main/java/com/kakao/batchapp/dataIngest/util/DateUtils.java
@@ -2,10 +2,11 @@ package com.kakao.batchapp.dataIngest.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 
 public class DateUtils {
 
-    private static DateTimeFormatter YYYY_MM_DD_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static DateTimeFormatter YYYY_MM_DD_FORMAT = DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(ResolverStyle.STRICT);;
 
     public static LocalDate toLocalDate(String stringDate) {
 
