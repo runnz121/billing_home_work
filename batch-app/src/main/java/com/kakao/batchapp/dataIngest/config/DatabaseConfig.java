1826537e8e90ddeb12d24aa961b3f0f0a528a4e2
runnz121 <runnz121@gmail.com>
2025-07-24 23:23:28 +0900

    [feat] db 환경설정 - jdbcTemplate 기반 db connection 작성

---
+++ batch-app/src/main/java/com/kakao/batchapp/dataIngest/config/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package com.kakao.batchapp.dataIngest.config;
+
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
