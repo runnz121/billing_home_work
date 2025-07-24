4a93e61a8b8a894a14e7dd3a114ed4cf521b1927
runnz121 <runnz121@gmail.com>
2025-07-24 23:32:09 +0900

    [feat] json 객체 바인드를 위한 맵퍼 설정 - ndJson 문자열 값을 객체로 바인딩 하기 위해 커스텀 맵퍼 설정 - \\/ 로 들어오는 케이스가 있어, 해당 부분 예외처리 조건 추가

---
+++ batch-app/src/main/java/com/kakao/batchapp/dataIngest/mapper/JsonCustomLineMapper.java
@@ -0,0 +1,35 @@
+package com.kakao.batchapp.dataIngest.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakao.batchapp.dataIngest.domain.model.MusicData;
+import org.springframework.batch.item.file.LineMapper;
+
+public class JsonCustomLineMapper implements LineMapper<MusicData> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String SLASH_PLACEHOLDER = "__SLASH__";
+
+    @Override
+    public MusicData mapLine(String line, int lineNumber) throws Exception {
+
+        String prepped = line.replace("\\/", SLASH_PLACEHOLDER);
+
+        // jackson 파싱
+        MusicData data = objectMapper.readValue(prepped, MusicData.class);
+
+        // POJO 필드에 담긴 토큰만 다시 \/ 로 복원
+        if (data.getSong() != null) {
+            data.setSong(data.getSong().replace(SLASH_PLACEHOLDER, "\\/"));
+        }
+
+        if (data.getAlbum() != null) {
+            data.setAlbum(data.getAlbum().replace(SLASH_PLACEHOLDER, "\\/"));
+        }
+
+        if (data.getArtist() != null) {
+            data.setArtist(data.getArtist().replace(SLASH_PLACEHOLDER, "\\/"));
+        }
+
+        return data;
+    }
+}
