1ed8957355a07d61a38c61340b7a77cb562cd809
runnz121 <runnz121@gmail.com>
2025-07-26 13:29:45 +0900

    [test] 테스트 유틸 생성 - json 파일을 객체로 맵핑하도록 testUtil 클래스 생성

---
+++ api-service/src/test/java/com/testUtils/TestUtils.java
@@ -0,0 +1,38 @@
+package com.testUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class TestUtils {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T fileToObject(String filePath, Class<T> clazz) throws JsonProcessingException {
+        String payload = asString(filePath);
+        return objectMapper.readValue(payload, clazz);
+    }
+
+    public static String asString(Resource resource) {
+
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public static String asString(String classPath) {
+
+        return asString(new ClassPathResource(classPath));
+    }
+}
